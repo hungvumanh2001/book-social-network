b36f4de8995248fe22eb5e2e79ceec718c06cbf3
DESKTOP-9O360Q0\SEATECH <hungvm@seatechit.com.vn>
2025-01-10 17:36:03 +0700

    authen gateway

--- identity-service/src/main/java/com/identity/configuration/CustomJwtDecoder.java
+++ identity-service/src/main/java/com/identity/configuration/CustomJwtDecoder.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.util.Objects;
 import javax.crypto.spec.SecretKeySpec;
 
+import com.nimbusds.jwt.SignedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -31,23 +32,36 @@ public class CustomJwtDecoder implements JwtDecoder {
 
     @Override
     public Jwt decode(String token) throws JwtException {
-
+//        remove do da verify token o phia service api gateway
+//        try {
+//            var response = authenticationService.introspect(
+//                    IntrospectRequest.builder().token(token).build());
+//
+//            if (!response.isValid()) throw new JwtException("Token invalid");
+//        } catch (JOSEException | ParseException e) {
+//            throw new JwtException(e.getMessage());
+//        }
+//
+//        if (Objects.isNull(nimbusJwtDecoder)) {
+//            SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+//            nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec)
+//                    .macAlgorithm(MacAlgorithm.HS512)
+//                    .build();
+//        }
+//
+//        return nimbusJwtDecoder.decode(token);
         try {
-            var response = authenticationService.introspect(
-                    IntrospectRequest.builder().token(token).build());
+            SignedJWT signedJWT = SignedJWT.parse(token);
 
-            if (!response.isValid()) throw new JwtException("Token invalid");
-        } catch (JOSEException | ParseException e) {
-            throw new JwtException(e.getMessage());
+            return new Jwt(
+                    token,
+                    signedJWT.getJWTClaimsSet().getIssueTime().toInstant(),
+                    signedJWT.getJWTClaimsSet().getExpirationTime().toInstant(),
+                    signedJWT.getHeader().toJSONObject(),
+                    signedJWT.getJWTClaimsSet().getClaims()
+                    );
+        } catch (ParseException e) {
+            throw new JwtException("Token invalid");
         }
-
-        if (Objects.isNull(nimbusJwtDecoder)) {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
-            nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec)
-                    .macAlgorithm(MacAlgorithm.HS512)
-                    .build();
-        }
-
-        return nimbusJwtDecoder.decode(token);
     }
 }
